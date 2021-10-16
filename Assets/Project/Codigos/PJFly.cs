929d6aa714c1a433bc3a1f928415f67a7d458c63
DESKTOP-VITOR\Vitor <vitorlima.0811@gmail.com>
2021-10-16 20:22:36 -0300

    Animação de vôo

--- Assets/Project/Codigos/PJFly.cs
+++ Assets/Project/Codigos/PJFly.cs
@@ -6,17 +6,20 @@ public class PJFly : MonoBehaviour
 {
     [SerializeField] private Vector2 FlyForce;
     private Rigidbody2D _rb;
+    private Animator _anim;
 
     void Start()
     {
         Application.targetFrameRate = 60;
         _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
     }
 
    public void Fly()
     {
         _rb.velocity = Vector2.zero;
         _rb.AddForce(FlyForce, ForceMode2D.Impulse);
+        _anim.SetBool("isFlying", true);
     }
     void Update()
     {
