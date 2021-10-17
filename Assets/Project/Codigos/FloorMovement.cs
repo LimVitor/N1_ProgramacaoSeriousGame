3c5032cf39d8a090c9116a5684c85506fcc9ad6e
DESKTOP-VITOR\Vitor <vitorlima.0811@gmail.com>
2021-10-16 22:24:11 -0300

    Fix - movimento do chão

--- Assets/Project/Codigos/FloorMovement.cs
+++ Assets/Project/Codigos/FloorMovement.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class FloorMovement : MonoBehaviour
 {
-    [Header("Floor")]
     [SerializeField] private int speed;
     [SerializeField] private Transform[] floor;
     [SerializeField] private float heigth;
-    public void Update()
+
+    public void Start()
+    {
+        this.enabled = false;
+    }
+    public void floorMovement()
     {
         foreach (Transform f in floor)
         {
+            this.enabled = true;
             f.Translate(new Vector2(0, speed * Time.deltaTime));
             if (f.position.y < heigth * -1)
             {
-                Destroy(this.gameObject);
+                Destroy(gameObject);
             }
         }
+    }
+
+    public void Update()
+    {
+        floorMovement();
     }
 }
--- Assets/Project/Codigos/PJFly.cs
+++ Assets/Project/Codigos/PJFly.cs
@@ -4,23 +4,22 @@ using UnityEngine;
 
 public class PJFly : MonoBehaviour
 {
-    [Header("PJ")]
     [SerializeField] private Vector2 FlyForce;
     private Rigidbody2D _rb;
     private Animator _anim;
 
-    [SerializeField] private FloorMovement floorMovement;
+    
 
     void Start()
     {
-       
+        
         _rb = GetComponent<Rigidbody2D>();
         _anim = GetComponent<Animator>();
     }
 
    public void Fly()
     {
-        floorMovement.enabled = true;
+       
         _rb.velocity = Vector2.zero;
         _rb.AddForce(FlyForce, ForceMode2D.Impulse);
         _anim.SetBool("isFlying", true);
