e794eb4d9f51169e5d7904f990b490cc146e7cf3
DESKTOP-VITOR\Vitor <vitorlima.0811@gmail.com>
2021-10-21 15:59:54 -0300

    Settings menu script; Settings menu config.; Menus finalizados

---
+++ Assets/Project/Codigos/SettingsMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+   [SerializeField] private AudioMixer audioMixer;
+   public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume",volume);
+    }
+
+    public void Set30FPS()
+    {
+        Application.targetFrameRate = 30;
+
+    }
+
+    public void Set60FPS()
+    {
+        Application.targetFrameRate = 60;
+        
+    }
+}
